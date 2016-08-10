cde7bc25145b417bf5deea9c1500c28dc9b51f89
marci <marci@DESKTOP-31NVLGM>
2016-08-10 19:11:52 +0200

    Add project files.

---
+++ LockedCounter/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LockedCounter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            SystemEvents.SessionSwitch += SystemEvents_SessionSwitch;
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            SystemEvents.SessionSwitch -= SystemEvents_SessionSwitch;
+        }
+
+        private void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            Debug.Write(DateTime.Now);
+            if (e.Reason == SessionSwitchReason.SessionLock)
+            {
+                Debug.WriteLine("Locked");
+            } else if (e.Reason == SessionSwitchReason.SessionUnlock)
+            {
+                Debug.WriteLine("Unlocked");
+            }
+        }
+    }
+}
---
+++ LockedCounter/TimeCounter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace LockedCounter
+{
+    public class TimeCounter: VMBase
+    {
+        private Timer timer;
+
+        private TimeSpan _unlockedTime;
+
+        public TimeSpan UnlockedTime
+        {
+            get { return _unlockedTime; }
+            set {
+                _unlockedTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private TimeSpan _lockedTime;
+
+        public TimeSpan LockedTime
+        {
+            get { return _lockedTime; }
+            set {
+                _lockedTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
